53da88cb2e0c353bb7ef1ad34e9d33b1991ebf6e
Francessmarty <100991131+Francessmarty@users.noreply.github.com>
2026-02-28 11:42:23 +0000

    Fix 04-deploy-all.sh and corrected screnshot

--- 01-multi-tier-architecture/scripts/04-deploy-all.sh
+++ 01-multi-tier-architecture/scripts/04-deploy-all.sh
@@ -4,8 +4,8 @@ set -e
 echo "Starting full deployment."
 
 ./scripts/01-deploy-network.sh
-./scripts/02-deploy.nsg.sh
-./scripts/03-deploy.vm.sh
+./scripts/02-deploy-nsg.sh
+./scripts/03-deploy-vms.sh
 ./scripts/05-verify.sh
 
 echo "Full deployment completed successfully."
